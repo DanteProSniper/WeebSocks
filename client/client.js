14d11454e1ac0ba8b9e4525300d67aa3b890d688
Dante Åkring <danakr0609@edu.halmstad.se>
2025-01-24 14:24:24 +0100

    förstår typ det mesta nu, gör lite css

--- client/client.js
+++ client/client.js
@@ -2,7 +2,31 @@
 
 const clientSocket = io();
 
+//om jag vill se hela klient objektet
 console.log(clientSocket);
+
+
+
+
+
+
+document.querySelector("#sendBtn").addEventListener("click", handleInput);
+document.querySelector("#input").addEventListener("keyup", event=>{
+
+
+    console.log(event);
+    if(event.keyCode == 13) handleInput();
+
+
+})
+function handleInput(event){
+
+    let input = document.querySelector("#input").value.trim();
+
+    if(!input) return
+
+   sendMessage(input)
+}
 
 
 
@@ -18,8 +42,25 @@ clientSocket.on("chat", function(obj){
 
     console.log(obj);
 
+    printMessage(obj);
+
 })
 
 clientSocket.on("con", function(msg){
     console.log(msg);
 })
+
+
+
+function printMessage(obj){
+
+    let div = document.createElement("div");
+    let p = document.createElement("p");
+    p.innerText = obj.msg;
+    div.appendChild(p)
+
+    document.querySelector(".chatContainer").appendChild(div);
+
+
+
+}
