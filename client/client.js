3b968616642ed3f3373cda57b0f1534eca3b978f
Dante Åkring <danakr0609@edu.halmstad.se>
2025-01-31 14:14:02 +0100

    har fixat lite mer css och så :)

--- client/client.js
+++ client/client.js
@@ -1,73 +1,54 @@
-
-
 const clientSocket = io();
 
 //om jag vill se hela klient objektet
 console.log(clientSocket);
 
-
-
-
-
 //gör addeventlistener för den andr aockså
-document.querySelectorAll(".sendBtn").forEach(btn=>btn.addEventListener("click", handleInput));
-document.querySelectorAll(".input").forEach(btn=>btn.addEventListener("keyup", event=>{
+document
+  .querySelectorAll(".sendBtn")
+  .forEach((btn) => btn.addEventListener("click", handleInput));
+document.querySelectorAll(".input").forEach((btn) =>
+  btn.addEventListener("keyup", (event) => {
+    if (event.keyCode == 13 && event.shiftKey == false) handleInput(event);
+  })
+);
+function handleInput(event) {
+  let roomID = event.srcElement.parentElement.parentElement.id;
 
+  let input = document
+    .getElementById(roomID)
+    .querySelector(".input")
+    .value.trim();
 
-    
-    if(event.keyCode == 13 && event.shiftKey == false) handleInput(event);
+  if (!input) return;
 
+  document.getElementById(roomID).querySelector(".input").value = "";
 
-}))
-function handleInput(event){
-
-    let roomID = event.srcElement.parentElement.parentElement.id;
-
-    let input = document.getElementById(roomID).querySelector(".input").value.trim();
-    
-    if(!input) return;
-
-    document.getElementById(roomID).querySelector(".input").value = "";
-
-    
-
-    sendMessage(input, roomID);
+  sendMessage(input, roomID);
 }
 
-
-
-function sendMessage(msg, roomID){
-
-
-    clientSocket.emit("chat", msg, roomID);
-
+function sendMessage(msg, roomID) {
+  clientSocket.emit("chat", msg, roomID);
 }
 
+clientSocket.on("chat", function (obj) {
+  console.log(obj);
+ 
+  printMessage(obj);
+});
 
-clientSocket.on("chat", function(obj){
+function printMessage(obj) {
+  let div = document.createElement("div");
+  let p = document.createElement("p");
+  p.innerText = obj.msg;
+  div.appendChild(p);
 
-    console.log(obj);
-
-    printMessage(obj);
-
-})
-
-function printMessage(obj){
-
-    let div = document.createElement("div");
-    let p = document.createElement("p");
-    p.innerText = obj.msg;
-    div.appendChild(p)
-
-    document.getElementById(obj.roomID).querySelector(".messageArea").appendChild(div);
-
-
-
+  document
+    .getElementById(obj.roomID)
+    .querySelector(".messageArea")
+    .appendChild(div);
 }
 
-clientSocket.on("con", function(msg){
-    console.log(msg);
-})
-
-
-
+clientSocket.on("con", function (msg) {
+  console.log(msg);
+});
--- index.js
+++ index.js
@@ -3,40 +3,30 @@ const app = express();
 
 app.use(express.static("client"));
 
-const {createServer} = require("http");
-const {Server} = require("socket.io");
+const { createServer } = require("http");
+const { Server } = require("socket.io");
 
 const server = createServer(app);
 const io = new Server(server);
 
 const port = 3400;
-server.listen(port, _=>{console.log(`http://localhost:${port}`)});
-
-app.get("/", (req, res)=>{
-   
-    res.sendFile(__dirname + "/chat.html");
-
+server.listen(port, (_) => {
+  console.log(`http://localhost:${port}`);
 });
 
-
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/chat.html");
+});
 
 io.on("connection", handleConnection);
 
+function handleConnection(socket) {
+  console.log("A user connected, " + socket.id);
 
-function handleConnection(socket){
+  io.emit("con", socket.id + " connected");
 
-    console.log("A user connected, "+socket.id);
-
-    io.emit("con", socket.id + " connected");
-
-
-    socket.on("chat", function(msg, roomID){
-
-        console.log(socket.id + ": " + msg + " in room " + roomID);
-        io.emit("chat", {id:socket.id, msg, roomID});
-
-    });
-
-
-
+  socket.on("chat", function (msg, roomID) {
+    console.log(socket.id + ": " + msg + " in room " + roomID);
+    io.emit("chat", { id: socket.id, msg, roomID });
+  });
 }
